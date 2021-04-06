6376fbb04f5bfae72dae5a8190f2cb48d121b908
VinuBalagopalAP <vinubalagopalap@gmail.com>
2021-04-07 01:25:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/nice_ui_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nice_ui_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:nice_ui_login/screens/homepage.dart';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
